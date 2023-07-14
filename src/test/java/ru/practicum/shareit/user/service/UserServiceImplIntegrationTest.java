dfdf6605b7d71503836f227c0d46bad050b0a6c5
Artem <ZherebtsovAA@yandex.ru>
2023-07-14 19:42:50 +0900

    test: add tests

---
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceImplIntegrationTest.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserServiceImplIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
