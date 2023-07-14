a898222d6afe6a145dcaa097cc74d3a9f4a98a57
Artem <ZherebtsovAA@yandex.ru>
2023-07-14 19:27:45 +0900

    test: add tests

---
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingResponseDtoJsonTest.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.booking.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JsonTest
+class BookingResponseDtoJsonTest {
+
+    @Autowired
+    private JacksonTester<BookingResponseDto> json;
+
+    @Test
+    void testSerialization() throws Exception {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = start.plusDays(1);
+        BookingResponseDto bookingResponseDto = new BookingResponseDto(1L, start, end, 1L);
+
+        JsonContent<BookingResponseDto> result = json.write(bookingResponseDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.start").isNotNull();
+        assertThat(result).extractingJsonPathStringValue("$.end").isNotNull();
+        assertThat(result).extractingJsonPathNumberValue("$.bookerId").isEqualTo(1);
+    }
+
+}
