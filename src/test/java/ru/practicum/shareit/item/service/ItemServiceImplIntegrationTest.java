eea62d535d4ca7b1e2532a6dc2d4bf8c0c660cb1
Artem <ZherebtsovAA@yandex.ru>
2023-07-15 20:04:22 +0900

    test: add tests

---
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceImplIntegrationTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.item.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class ItemServiceImplIntegrationTest {
+    @Autowired
+    ItemService itemService;
+
+    @Test
+    void test() {
+
+    }
+
+}
---
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplIntegrationTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.request.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class ItemRequestServiceImplIntegrationTest {
+    @Autowired
+    ItemRequestService itemRequestService;
+
+    @Test
+    void test() {
+
+    }
+
+}
